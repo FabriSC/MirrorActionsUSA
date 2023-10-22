4de5e5e714b941b7b12bbc89ee6b4fe328351282
FabriSC <u22216510@utp.edu.pe>
2023-10-22 13:38:03 -0500

    Activando: 1 horas,numero de activacion:

--- botusa.sh
+++ botusa.sh
@@ -1,22 +1,22 @@
 
 echo "Iniciando el bot..."
 if [[ "1" = "$1" ]]; then
-$hora=3600
+$hora = 3600
 fi
 if [[ "2" = "$1" ]]; then
-$hora=7200
+$hora = 7200
 fi
 if [[ "3" = "$1" ]]; then
-$hora=10800
+$hora = 10800
 fi
 if [[ "4" = "$1" ]]; then
-$hora=14400
+$hora = 14400
 fi
 if [[ "5" = "$1" ]]; then
-$hora=18000
+$hora = 18000
 fi
 if [[ "6" = "$1" ]]; then
-$hora=21600
+$hora = 21600
 else
 echo "Error: solo se puede establecer de 1 a 6 horas, aplicando cambios automaticos..."
 echo "inicie de nuevo el codigo"
